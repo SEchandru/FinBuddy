009e8e28ffc205d3cc9c044cb755dc7318f6ceca
CHANDRABAL C <119514324+SEchandru@users.noreply.github.com>
2026-07-01 21:48:48 +0530

    chore: update vercel routing rules

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -1,14 +1,14 @@
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <HashRouter>
+  <BrowserRouter>
     <AuthProvider>
       <App />
     </AuthProvider>
-  </HashRouter>
+  </BrowserRouter>
 );
